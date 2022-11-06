a91d22552835f25b7da2fe7a327d719c2668627c
Pranav <pranavparge@gmail.com>
2022-11-06 15:45:35 +0530

    Projects Phase II

---
+++ src/components/ProjectDetails/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./index.css";
+
+function ProjectDetails() {
+  return <div className="Details">Project Details</div>;
+}
+
+export default ProjectDetails;
--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -3,7 +3,10 @@ import "./index.css";
 import Separator from "../Separator";
 import Browser from "../Browser";
 import Twilio from "../../assets/Twilio.png";
+import Bank from "../../assets/Bank.png";
 import Arrow from "../../assets/right-arrow.svg";
+import ReactLogo from "../../assets/react-logo.svg";
+import Typewriter from "typewriter-effect";
 
 function Projects({ projectsRef }) {
   const Project = ({ projectName, projectObj, projectBrowser }) => (
@@ -42,50 +45,51 @@ function Projects({ projectsRef }) {
         projectName={"Contact Center"}
         projectObj={"Business to Business Contact Center"}
         projectBrowser={
+          <img className="project-img" src={Twilio} alt="Twilio"></img>
+        }
+      />
+      <Separator />
+      <Project
+        projectName={"Payments"}
+        projectObj={"Payments Portal for a Bank"}
+        projectBrowser={
           <img
-            style={{
-              height: "80vh",
-              width: "80vw",
-              borderBottomLeftRadius: "0.5vw",
-              borderBottomRightRadius: "0.5vw",
-            }}
-            src={Twilio}
-            alt="Twilio"
+            style={{ height: "80vh" }}
+            className="project-img"
+            src={Bank}
+            alt="Bank"
           ></img>
         }
       />
       <Separator />
       <Project
-        projectName={"Contact Center"}
-        projectObj={"Business to Business Contact Center"}
+        projectName={"Mainframe to React Migration"}
+        projectObj={"Bank Administration website"}
         projectBrowser={
-          <img
+          <div
             style={{
-              height: "80vh",
-              width: "80vw",
-              borderBottomLeftRadius: "0.5vw",
-              borderBottomRightRadius: "0.5vw",
+              background: "#000",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              color: "#2afa05",
+              fontSize: "calc(5vw + 2vmin)",
             }}
-            src={Twilio}
-            alt="Twilio"
-          ></img>
-        }
-      />
-      <Separator />
-      <Project
-        projectName={"Contact Center"}
-        projectObj={"Business to Business Contact Center"}
-        projectBrowser={
-          <img
-            style={{
-              height: "80vh",
-              width: "80vw",
-              borderBottomLeftRadius: "0.5vw",
-              borderBottomRightRadius: "0.5vw",
-            }}
-            src={Twilio}
-            alt="Twilio"
-          ></img>
+            className="project-img"
+          >
+            <Typewriter
+              options={{ loop: true }}
+              onInit={(typewriter) => {
+                typewriter.typeString("Mainframe").start().pauseFor(5000);
+              }}
+            />
+            <span style={{ margin: "0 0 0 5vw", color: "white" }}>-&gt;</span>
+            <img
+              src={ReactLogo}
+              className="react-logo App-logo"
+              alt="React logo"
+            />
+          </div>
         }
       />
     </div>
